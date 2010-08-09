4671bd9fa3c844701c9320cf83ab6a9c8a709cf5
Rob Clark <rob@ti.com>
2010-08-08 23:07:50 -0500

    VP8: Implement bicubic MC functions (put_vp8_epel*)
     + put_vp8_epel16_h6v6_neon
     + put_vp8_epel16_h6_neon
     + put_vp8_epel16_v6_neon
     + put_vp8_epel8_v6_neon
     + put_vp8_epel8_h6_neon
     + put_vp8_epel8_h6v6_neon
     + put_vp8_epel8_v4_neon
     + put_vp8_epel8_h4_neon
     + put_vp8_epel8_h4v4_neon
     + put_vp8_epel8_h6v4_neon
     + put_vp8_epel8_h4v6_neon
     + put_vp8_pixels16_neon
     + put_vp8_pixels8_neon
     + put_vp8_pixels4_neon

--- libavcodec/arm/vp8dsp_init_arm.c
+++ libavcodec/arm/vp8dsp_init_arm.c
@@ -44,6 +44,61 @@ void vp8_v_loop_filter_simple_neon(uint8_t *dst, int stride, int flim);
 void vp8_h_loop_filter_simple_neon(uint8_t *dst, int stride, int flim);
 
 
+/*
+ * put_vp8_*_pixels_tab functions:
+ */
+void put_vp8_pixels16_neon(uint8_t *dst, int dststride, uint8_t *src,
+        int srcstride, int h, int x, int y);
+void put_vp8_pixels8_neon(uint8_t *dst, int dststride, uint8_t *src,
+        int srcstride, int h, int x, int y);
+void put_vp8_pixels4_neon(uint8_t *dst, int dststride, uint8_t *src,
+        int srcstride, int h, int x, int y);
+void put_vp8_epel16_v6_neon(uint8_t * dst, int dststride, uint8_t * src,
+        int srcstride, int h, int mx, int my);
+void put_vp8_epel16_h6_neon(uint8_t * dst, int dststride, uint8_t * src,
+        int srcstride, int h, int mx, int my);
+void put_vp8_epel16_h6v6_neon(uint8_t * dst, int dststride, uint8_t * src,
+        int srcstride, int h, int mx, int my);
+void put_vp8_epel8_v6_neon (uint8_t * dst, int dststride, uint8_t * src,
+        int srcstride, int h, int mx, int my);
+void put_vp8_epel8_h6_neon (uint8_t * dst, int dststride, uint8_t * src,
+        int srcstride, int h, int mx, int my);
+void put_vp8_epel8_h6v6_neon(uint8_t * dst, int dststride, uint8_t * src,
+        int srcstride, int h, int mx, int my);
+void put_vp8_epel8_v4_neon(uint8_t * dst, int dststride, uint8_t * src,
+        int srcstride, int h, int mx, int my);
+void put_vp8_epel8_h4_neon(uint8_t * dst, int dststride, uint8_t * src,
+        int srcstride, int h, int mx, int my);
+void put_vp8_epel8_h4v4_neon(uint8_t * dst, int dststride, uint8_t * src,
+        int srcstride, int h, int mx, int my);
+void put_vp8_epel8_h6v4_neon(uint8_t * dst, int dststride, uint8_t * src,
+        int srcstride, int h, int mx, int my);
+void put_vp8_epel8_h4v6_neon(uint8_t * dst, int dststride, uint8_t * src,
+        int srcstride, int h, int mx, int my);
+
+#define VP8_MC_FUNC(IDX, SIZE) \
+    dsp->put_vp8_epel_pixels_tab[IDX][0][0] = put_vp8_pixels ## SIZE ## _neon; \
+//    dsp->put_vp8_epel_pixels_tab[IDX][0][1] = put_vp8_epel ## SIZE ## _h4_neon; \
+//    dsp->put_vp8_epel_pixels_tab[IDX][0][2] = put_vp8_epel ## SIZE ## _h6_neon; \
+//    dsp->put_vp8_epel_pixels_tab[IDX][1][0] = put_vp8_epel ## SIZE ## _v4_neon; \
+//    dsp->put_vp8_epel_pixels_tab[IDX][1][1] = put_vp8_epel ## SIZE ## _h4v4_neon; \
+//    dsp->put_vp8_epel_pixels_tab[IDX][1][2] = put_vp8_epel ## SIZE ## _h6v4_neon; \
+//    dsp->put_vp8_epel_pixels_tab[IDX][2][0] = put_vp8_epel ## SIZE ## _v6_neon; \
+//    dsp->put_vp8_epel_pixels_tab[IDX][2][1] = put_vp8_epel ## SIZE ## _h4v6_neon; \
+//    dsp->put_vp8_epel_pixels_tab[IDX][2][2] = put_vp8_epel ## SIZE ## _h6v6_neon
+
+#define VP8_BILINEAR_MC_FUNC(IDX, SIZE) \
+    dsp->put_vp8_bilinear_pixels_tab[IDX][0][0] = put_vp8_pixels ## SIZE ## _neon; \
+//    dsp->put_vp8_bilinear_pixels_tab[IDX][0][1] = put_vp8_bilinear ## SIZE ## _h_neon; \
+//    dsp->put_vp8_bilinear_pixels_tab[IDX][0][2] = put_vp8_bilinear ## SIZE ## _h_neon; \
+//    dsp->put_vp8_bilinear_pixels_tab[IDX][1][0] = put_vp8_bilinear ## SIZE ## _v_neon; \
+//    dsp->put_vp8_bilinear_pixels_tab[IDX][1][1] = put_vp8_bilinear ## SIZE ## _hv_neon; \
+//    dsp->put_vp8_bilinear_pixels_tab[IDX][1][2] = put_vp8_bilinear ## SIZE ## _hv_neon; \
+//    dsp->put_vp8_bilinear_pixels_tab[IDX][2][0] = put_vp8_bilinear ## SIZE ## _v_neon; \
+//    dsp->put_vp8_bilinear_pixels_tab[IDX][2][1] = put_vp8_bilinear ## SIZE ## _hv_neon; \
+//    dsp->put_vp8_bilinear_pixels_tab[IDX][2][2] = put_vp8_bilinear ## SIZE ## _hv_neon
+
+
 av_cold void ff_vp8dsp_init_arm(VP8DSPContext *dsp)
 {
     if (HAVE_NEON) {
@@ -61,5 +116,26 @@ av_cold void ff_vp8dsp_init_arm(VP8DSPContext *dsp)
         dsp->vp8_v_loop_filter_simple = vp8_v_loop_filter_simple_neon;
         dsp->vp8_h_loop_filter_simple = vp8_h_loop_filter_simple_neon;
 #endif
+
+        VP8_MC_FUNC(0, 16);
+        VP8_MC_FUNC(1, 8);
+        VP8_MC_FUNC(2, 4);
+
+        VP8_BILINEAR_MC_FUNC(0, 16);
+        VP8_BILINEAR_MC_FUNC(1, 8);
+        VP8_BILINEAR_MC_FUNC(2, 4);
+
+        // XXX
+        dsp->put_vp8_epel_pixels_tab[0][2][0] = put_vp8_epel16_v6_neon;
+        dsp->put_vp8_epel_pixels_tab[0][0][2] = put_vp8_epel16_h6_neon;
+        dsp->put_vp8_epel_pixels_tab[0][2][2] = put_vp8_epel16_h6v6_neon;
+        dsp->put_vp8_epel_pixels_tab[1][2][0] = put_vp8_epel8_v6_neon;
+        dsp->put_vp8_epel_pixels_tab[1][0][2] = put_vp8_epel8_h6_neon;
+        dsp->put_vp8_epel_pixels_tab[1][2][2] = put_vp8_epel8_h6v6_neon;
+        dsp->put_vp8_epel_pixels_tab[1][1][0] = put_vp8_epel8_v4_neon;
+        dsp->put_vp8_epel_pixels_tab[1][0][1] = put_vp8_epel8_h4_neon;
+        dsp->put_vp8_epel_pixels_tab[1][1][1] = put_vp8_epel8_h4v4_neon;
+        dsp->put_vp8_epel_pixels_tab[1][1][2] = put_vp8_epel8_h6v4_neon;
+        dsp->put_vp8_epel_pixels_tab[1][2][1] = put_vp8_epel8_h4v6_neon;
     }
 }
